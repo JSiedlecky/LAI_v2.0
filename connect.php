4217bea2f08a9c5c2e45c4a1864284fbf932f134
JSiedlecky <jakub.w.siedlecki@gmail.com>
2016-02-04 01:08:17 +0100

    wiecej PDO

--- connect.php
+++ connect.php
@@ -1,28 +1,30 @@
 <?php
     
-//    $host = "http://server.marox44.pl";
-//    $db = "lai";
-//    $db_user = "lai";
-//    $db_passwd = "lai";    
-//
-//    try {
-//        $conn = new PDO("mysql:host=$host;dbname=$db", $db_user, $db_passwd);
-//        // set the PDO error mode to exception
-//        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//        echo "Connected successfully"; 
-//    }catch(PDOException $e){
-//        echo "Connection failed: " . $e->getMessage();
-//    }
+    $host = "server.marox44.pl";
+    $port = 3306;
+    $db = "lai";
+    $db_user = "lai";
+    $db_passwd = "lai";
+    $conn = "";
 
-        try {
-            $dbh = new PDO('mysql:host=178.37.44.32;port=3306;dbname=lai', "lai", "lai");
-            foreach($dbh->query('SELECT * from news') as $row) {
-                print_r($row);
-            }
-            $dbh = null;
-        } catch (PDOException $e) {
-            print "Error!: " . $e->getMessage() . "<br/>";
-            die();
+    try {
+        $conn = new PDO("mysql:host=".$host.";port=".$port.";dbname=".$db, $db_user, $db_passwd);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully"; 
+    }catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+    
+echo "<br>";
+    
+        $sql = 'SELECT * FROM news';
+        foreach ($conn->query($sql) as $row) {
+            print $row['id'] . "&#09;";
+            print $row['title'] . "&#09;";
+            print $row['brief'] . "&#09;";
+            print $row['date'] . "<br><br>";
         }
+    
 
 ?>
