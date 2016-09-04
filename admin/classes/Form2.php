3867b6739bce94c8ccf590c9e913b4499109b833
JSiedlecky <jakub.w.siedlecki@gmail.com>
2016-09-04 20:54:11 +0200

    Więcej użyteczności w klasie Form2, css do niej
    Do dodania jeszcze pare rodzai inputów. Póki co to fajne to :3

--- admin/classes/Form2.php
+++ admin/classes/Form2.php
@@ -3,10 +3,14 @@
 class Form2 {
 
   private $content = "";
+  private $ajax = false;
 
   public function __construct($ajax = false, $method = "POST", $action = "#", $class = "default-form", $novalidate = false){
-    if($ajax) $form = '<form';
-      else $form = '<form method="'.$method.'" action="'.$action.'"';
+    if($ajax) {
+      $form = '<form';
+      $this->ajax = $ajax;
+    }
+    else $form = '<form method="'.$method.'" action="'.$action.'"';
 
     $form .= ' class="'.$class.'"';
 
@@ -16,55 +20,119 @@ class Form2 {
     $this->content .= $form;
   }
 
-  public function Textbox($name, $placeholder = "", $add = "") {
-    $input = $this->buildStandardInput('text',$name,$placeholder,$add);
+  #TEXT-ISH INPUTS
+
+  public function Textbox($name, $display_name, $placeholder = "", $add = "") {
+    $input = $this->buildStandardInput('text', $name, $display_name, $placeholder, $add);
 
     $this->content .= $input;
   }
 
-  public function Email($name, $placeholder = "", $add = "") {
-    $input = $this->buildStandardInput('email',$name,$placeholder,$add);
+  public function Email($name, $display_name, $placeholder = "", $add = "") {
+    $input = $this->buildStandardInput('email', $name, $display_name, $placeholder, $add);
 
     $this->content .= $input;
   }
 
-  public function Date($name, $placeholder = "", $add = ""){
-    $input = $this->buildStandardInput('date',$name,$placeholder,$add);
+  public function Password($name, $display_name, $placeholder = "", $add = ""){
+    $input = $this->buildStandardInput('password', $name, $display_name, $placeholder, $add);
 
     $this->content .= $input;
   }
 
-  public function DateMin($name, $placeholder = "", $min){
-    $add = 'min="'.$min.'"';
-    $input = $this->buildStandardInput('date', $name, $placeholder, $add);
-
-    $this->content .= $input;
-  }
-
-  public function DateMax($name, $placeholder = "", $max){
-    $add = 'max="'.$max.'"';
-    $input = $this->buildStandardInput('date', $name, $placeholder, $add);
-
-    $this->content .= $input;
-  }
-
-  public function DateMinMax($name, $placeholder = "", $min, $max){
-    $add = 'min="'.$min.'" max="'.$max.'"';
-    $input = $this->buildStandardInput('date', $name, $placeholder, $add);
-
-    $this->content .= $input;
-  }
-
-  private function buildStandardInput($type, $name, $placeholder, $add){
+  public function Textarea($name, $display_name, $placeholder = "", $rows = 7, $cols =  40){
     $input = '<div class="form_section">';
-      $input .= '<div class="textbox_name">'.$placeholder.'</div>';
-      $input .= '<input type="'.$type.'" name="'.$name.'" placeholder="'.$placeholder.'" '.$add.' />';
+      $input .= '<div class="input_name">'.$display_name.'</div>';
+      $input .= '<textarea name="'.$name.'" rows="'.$rows.'" cols="'.$cols.'">';
+        $input .= ($placeholder != "" ? $placeholder : '');
+      $input .= '</textarea>';
+    $input .= '</div';
+
+    $this->content .= $input;
+  }
+
+  #DATES
+
+  public function Date($name, $display_name){
+    $input = $this->buildStandardInput('date', $name, $display_name);
+
+    $this->content .= $input;
+  }
+
+  public function DateMin($name, $display_name, $min){
+    $add = 'min="'.$min.'"';
+    $input = $this->buildStandardInput('date', $name, $display_name, "", $add);
+
+    $this->content .= $input;
+  }
+
+  public function DateMax($name, $display_name, $max){
+    $add = 'max="'.$max.'"';
+    $input = $this->buildStandardInput('date', $name, $display_name, "", $add);
+
+    $this->content .= $input;
+  }
+
+  public function DateMinMax($name, $display_name, $min, $max){
+    $add = 'min="'.$min.'" max="'.$max.'"';
+    $input = $this->buildStandardInput('date', $name, $display_name, "", $add);
+
+    $this->content .= $input;
+  }
+
+  #CHOISES
+
+  public function Radio($name, $display_name, $data = [], $checked = "", $horizontal = false){
+    $input = '<div class="form_section">';
+      $input .= '<div class="input_name">'.$display_name.'</div>';
+      foreach($data as $v => $d){
+        $input .= '<input type="radio" value="'.$v.'" '.($checked == $v ? 'checked' : '').'> '.$d.($horizontal != true ? '<br>' : ' ');
+      }
+    $input .='</div>';
+
+    $this->content .= $input;
+  }
+
+  public function Select($name, $display_name, $data = [], $selected = "", $multiple = flase, $size = 0){
+    $input = '<div class="form_section">';
+      $input .= '<div class="input_name">'.$display_name.'</div>';
+      $input .= '<select name="'.$name.'"'.($size > 0 ? 'size="'.$size.'"' : '').($multiple ? ' multiple' : '').'>';
+        foreach($data as $v => $d){
+          $input .= '<option'.($v == $selected ? ' selected' : '').'>';
+            $input .= $v;
+          $input .= '</option>';
+        }
+      $input .= '</select>';
+    $input .= '</div>';
+
+    $this->content .= $input;
+  }
+
+  #BUTTONS
+
+  public function ResetBtn($value){
+    $input = '<div class="form_section">';
+      $input .= '<input type="reset" value="'.$value.'">';
+    $input .='</div>';
+
+    $this->content .= $input;
+  }
+
+  private function buildStandardInput($type, $name, $display_name, $placeholder = "", $add = ""){
+    $input = '<div class="form_section">';
+      $input .= '<div class="input_name">'.$display_name.'</div>';
+      $input .= '<input type="'.$type.'" name="'.$name.'"'.($placeholder != "" ? ' placeholder="'.$placeholder.'"' : '').' '.$add.' />';
     $input .= '</div>';
 
     return $input;
   }
 
-  public function Render($debug = false){
+  public function Render($submit = "Wyślij", $debug = false){
+    if(!$this->ajax){
+      $this->content .= '<div class="form_section">';
+        $this->content .= '<input type="submit" value="'.$submit.'">';
+      $this->content .= '</div>';
+    }
     $this->content .= '</form>';
 
     if($debug){
--- admin/views/view.testing.php
+++ admin/views/view.testing.php
@@ -2,14 +2,30 @@
 
 $view->Header('ELO');
 
-$form = new Form2();
+$radiodata = [
+  "audi"    => "Audi",
+  "vw"      => "Volkswagen",
+  "porsche" => "Porsche"
+];
+
+$selectdata = [
+  "audi"    => "Audi",
+  "vw"      => "Volkswagen",
+  "porsche" => "Porsche"
+];
+
+$form = new Form2(false);
 
 $form->Textbox("textbox1", "Textbox");
 $form->Email("email1", "Email");
-$form->Date("date1","Date");
+$form->Date("date1", "Date");
 $form->DateMin("datemin1","DateMin","2001-01-01");
 $form->DateMax("datemax1","DateMax","2015-01-01");
 $form->DateMinMax("dateminmax1","DateMinMax","2015-01-01", "2017-01-01");
+$form->Textarea('textarea1','Textarea');
+$form->Radio('auto','Auto',$radiodata,'vw',true);
+$form->Select('auto2','Auto2',$selectdata,'vw',true, 2);
+$form->ResetBtn('Wyczyść');
 
 $view->AppendContent($form->Render());
 
